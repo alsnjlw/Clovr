6bbedca10e7c6623d2373ded53b3763b52a0a5e3
najla alisa <76194164+alsnjlw@users.noreply.github.com>
2024-11-22 21:11:26 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,23 +1,14 @@
 <?php
-// Connection to registration database (login_app)
-$reg_servername = "localhost";
-$reg_username = "root";
-$reg_password = "";
-$reg_dbname = "login_app"; // Registration database
+// Connection to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_app"; // Your single database
 
-$reg_conn = new mysqli($reg_servername, $reg_username, $reg_password, $reg_dbname);
-if ($reg_conn->connect_error) {
-    die("Connection failed: " . $reg_conn->connect_error);
-}
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Connection to login database (register_app)
-$login_servername = "localhost";
-$login_username = "root";
-$login_password = "";
-$login_dbname = "register_app"; // Login attempts database
-
-$login_conn = new mysqli($login_servername, $login_username, $login_password, $login_dbname);
-if ($login_conn->connect_error) {
-    die("Connection failed: " . $login_conn->connect_error);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
